3f172d3361ff6195d2971c628f0cf1af45361f68
Alexander <a.s.pyatkin@gmail.com>
2014-06-21 13:20:03 +0400

    Added CHANGES file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gang',
-    version='0.0.0',
+    version='0.0.1',
     description='Python dictionary with dot-style access',
     author='Alexander Pyatkin',
     author_email='asp@thexyz.net',
