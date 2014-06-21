b9288a8f60adce461dd72c29af006bc45da1bea6
Alexander <a.s.pyatkin@gmail.com>
2014-06-21 12:31:15 +0400

    Updated package format

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ from setuptools import setup, find_packages
 setup(
     name='gang',
     version='0.0.0',
-    description='Python dictionary with dot style access',
+    description='Python dictionary with dot-style access',
     author='Alexander Pyatkin',
     author_email='asp@thexyz.net',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     data_files=[],
     install_requires=[],
+    url='http://pypi.python.org/pypi/gang',
+    license='LICENSE'
 )
